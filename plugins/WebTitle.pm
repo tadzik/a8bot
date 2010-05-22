cc8989c711fcbc294b948d288a45ea378d6b3da4
lessmian <lessmian2@o2.pl>
2010-05-22 20:58:18 +0200

    Request's timeouts in WebTitle

--- plugins/WebTitle.pm
+++ plugins/WebTitle.pm
@@ -1,5 +1,7 @@
 package WebTitle;
 use LWP::Simple qw /get $ua/;
+use Sys::SigAction qw /timeout_call/;
+use vars qw /$site/;
 
 sub init {
 	return { publicmsg => \&pubmsg };
@@ -7,12 +9,17 @@ sub init {
 
 sub pubmsg {
 	my ($bot, $data) = @_;
+	my $timeout = 10;
 	if ($data->{msg} =~ /(http:\/\/[^ ]+)/) {
 		$ua->max_size(1024);
-		my $site = get($1);
-		my ($title) = $site =~ /<title>([^<]+)<\/title>/i;
-		$title =~ s/\n/ /g;
-		return "[ $title ]" if $title;
+		if (timeout_call($timeout, sub {$site = get($1)})){
+			return undef;
+		}
+		else {
+			my ($title) = $site =~ /<title>([^<]+)<\/title>/i;
+			$title =~ s/\n/ /g;
+			return "[ $title ]" if $title;
+		}
 	}
 	return undef;
 }
